f68fc60855328a2e7d6b321af1312b6378b16252
Gaby <1gabycamargo@gmail.com>
2026-08-10 20:21:06 -0300

    atividade 2 realizada

---
+++ 02-dinheiro.php
@@ -0,0 +1,15 @@
+<?php
+$dinheiro = 5000;
+
+if ($dinheiro < 1000) {
+    echo "Pobre";
+} elseif ($dinheiro < 5000) {
+    echo "Classe Média";
+} elseif ($dinheiro < 10000) {
+    echo "Riquinho"
+} elseif ($dinheiro < 100000) {
+    echo "Ricão";
+} else {
+    echo "Elon Musk";
+}
+?>
