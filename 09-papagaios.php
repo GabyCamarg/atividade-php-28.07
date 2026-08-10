28b331c8e4e1a4b9aecae68b9e1ba2a55375b065
Gaby <1gabycamargo@gmail.com>
2026-08-10 20:52:48 -0300

    Atividade 9 pronta

---
+++ 09-papagaios.php
@@ -0,0 +1,15 @@
+<?php
+
+$papagaios = [
+    "Papagaio-verdadeiro",
+    "Papagaio-do-mangue",
+    "Papagaio-de-peito-roxo"
+];
+
+$papagaios[] = "Papagaio-campeiro";
+
+foreach ($papagaios as $papagaio) {
+    echo $papagaio . "<br>";
+}
+
+?>
