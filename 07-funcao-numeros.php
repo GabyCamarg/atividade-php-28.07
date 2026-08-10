19d00cf86f1cf1f4dedad6049bef2323995ab836
Gaby <1gabycamargo@gmail.com>
2026-08-10 20:33:47 -0300

    Atividade 7 pronta

---
+++ 07-funcao-numeros.php
@@ -0,0 +1,16 @@
+<?php
+
+function calcular ($numero1, $numero2) {
+
+    $soma = $numero1 + $numero2;
+    $subtracao = $numero1 - $numero2
+
+    echo "Soma: $soma<br>";
+    echo "Subtracao: $subtracao<br>";
+    echo "Essa conta ficou perfeita!";
+
+}
+
+calcular (10,5);
+
+?>
